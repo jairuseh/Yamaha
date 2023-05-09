237ac78433266b4de2e57151ef291b877915602d
jai13 <jai@smartwave.ph>
2023-05-09 20:03:02 +0800

    re-create/refactor api handler

--- Yamaha/Api/ApiHandler.swift
+++ Yamaha/Api/ApiHandler.swift
@@ -9,31 +9,29 @@ import Foundation
 import Alamofire
 
 class ApiHandler {
-  
   static let sharedInstance = ApiHandler()
   
   func getProductList(handler: @escaping (_ apiData: [Product]) -> (Void)) {
-    
     let url = Constants.baseUrl + "filterAll?lim=11&p=1"
-    let headers: HTTPHeaders = [ "Authorization": Constants.token ]
+    let headers: HTTPHeaders = [ "Authorization": Constants.token]
     
     AF.request(url, method: .post, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).response { response in
       switch response.result {
       case .success(let data):
+        
         do {
           let jsonData = try JSONDecoder().decode(ProductListResponse.self, from: data!)
           print("Sample: \(jsonData.d)")
           handler(jsonData.d)
         } catch {
-          print("ERROR:")
+          print(error.localizedDescription)
           print(String(describing: error))
         }
       case .failure(let error):
-        print("ERROR:")
-        print(String(describing: error))
+        print(error.localizedDescription)
       }
     }
     
-    
   }
+  
 }
