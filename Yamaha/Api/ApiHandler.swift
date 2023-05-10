baacf0c1a189c1b2fd8abafdd91bd08c87a9e98f
jai13 <jai@smartwave.ph>
2023-05-10 15:51:05 +0800

    create api method for bolo

--- Yamaha/Api/ApiHandler.swift
+++ Yamaha/Api/ApiHandler.swift
@@ -57,4 +57,27 @@ class ApiHandler {
     
   }
   
+  func getBoloList(handler: @escaping (_ apiData: [Bolo]) -> (Void)) {
+    let url = Constants.baseUrl + "allWatchLists?p=1&lim=50"
+    let headers: HTTPHeaders = [ "Authorization": Constants.token]
+    
+    AF.request(url, method: .post, parameters: nil, encoding: URLEncoding.default, headers: headers, interceptor: nil).response { response in
+      switch response.result {
+      case .success(let data):
+        
+        do {
+          let jsonData = try JSONDecoder().decode(BoloListResponse.self, from: data!)
+          print("Sample: \(jsonData.d)")
+          handler(jsonData.d)
+        } catch {
+          print(error.localizedDescription)
+          print(String(describing: error))
+        }
+      case .failure(let error):
+        print(error.localizedDescription)
+      }
+    }
+    
+  }
+  
 }
